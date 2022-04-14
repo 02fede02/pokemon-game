7819c19e410ab703609d01dd635674056cdfcf49
02fede02 <fede.birman93@gmail.com>
2022-04-14 20:00:49 -0300

    Add images, style and change js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 
 const pokemon = ["Charmander", "Squirtirle", "Bulbasaur"];
 let playerWins;
+let playerScore = 0;
+let computerScore = 0;
+
+const charmander = document.querySelector(".charmander");
+const squirtirle = document.querySelector(".squirtirle");
+const bulbasaur = document.querySelector(".bulbasaur");
+
+const playerScoreMark = document.querySelector(".player-score");
+const computerScoreMark = document.querySelector(".computer-score");
+const result = document.querySelector(".result");
+
+charmander.addEventListener("click", () => {
+    const playerSelection = pokemon[0];
+	const computerChoosen = computerPlay();
+	playRound(playerSelection, computerChoosen);
+	game();
+})
+
+squirtirle.addEventListener("click", () => {
+    const playerSelection = pokemon[1];
+	const computerChoosen = computerPlay();
+	playRound(playerSelection, computerChoosen);
+	game();
+})
+
+bulbasaur.addEventListener("click", () => {
+    const playerSelection = pokemon[2];
+	const computerChoosen = computerPlay();
+	playRound(playerSelection, computerChoosen);
+	game();
+})
 
 function computerPlay() {
 	let numb = Math.floor(Math.random() * (3 - 0) + 0);
@@ -9,51 +40,55 @@ function computerPlay() {
 
 function playRound (playerSelection, computerSelection) {
 	if (playerSelection === computerSelection) {
-		console.log(`It's a draw, you choose the same ${playerSelection}`);
+		playerWins = undefined;
+		result.textContent = `It's a draw, you choose the same ${playerSelection}`;
 	}
 	else if (playerSelection === "Charmander" && computerSelection === "Squirtirle" ) {
 		playerWins = false;
-		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+		result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
 	} else if (playerSelection === "Charmander" && computerSelection === "Bulbasaur") {
 		playerWins = true;
-		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+		result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
 	}  else if (playerSelection === "Squirtirle" && computerSelection === "Bulbasaur") {
 		playerWins = false;
-		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+		result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
 	} else if (playerSelection === "Squirtirle" && computerSelection === "Charmander") {
 		playerWins = true;
-		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+		result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
 	}  else if (playerSelection === "Bulbasaur" && computerSelection === "Charmander") {
 		playerWins = false;
-		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+		result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
 	} else if (playerSelection === "Bulbasaur" && computerSelection === "Squirtirle") {
-		console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+		result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
 		playerWins = true;
 	}
 }
 
 function game() {
-	let playerScore = 0;
-	let computerScore = 0;
-	for (let i = 0; i < 5; i++) {	
-		const numb = parseInt(prompt(`Choose a pokemon(enter the number):\n1: Charmander \n2: Squirtirle\n3: Bulbasaur`));
-		const playerSelection = pokemon[numb - 1];
-		const computerChoosen = computerPlay();
-		playRound(playerSelection, computerChoosen);
+	//let playerScore = 0;
+	//let computerScore = 0;
+	//for (let i = 0; i < 5; i++) {	
+		//const numb = parseInt(prompt(`Choose a pokemon(enter the number):\n1: Charmander \n2: Squirtirle\n3: Bulbasaur`));
+		//const playerSelection = pokemon[numb - 1];
+		//const computerChoosen = computerPlay();
+		//playRound(playerSelection, computerChoosen);
 		if (playerWins === true){
 			playerScore ++;
+            playerScoreMark.textContent = `Player score: ${playerScore}`;
 		} else if (playerWins === false) {
 			computerScore++;
+            computerScoreMark.textContent = `Computer score: ${computerScore}`
 		} 
+		console.log(playerWins, playerScore, computerScore)
+	// Habria que cerrar el for con } 
+	if (playerScore === 5) {
+		result.textContent = "You won the game";
+	} else if (computerScore === 5) {
+		result.textContent = "You lose the game";
 	}
-	if (playerScore > computerScore) {
-		console.log("You won");
-	} else if (playerScore < computerScore) {
-		console.log("You lose");
-	}
-		else {
-		console.log("It's a draw");
-	}
+		//else {
+		//console.log("It's a draw");
+	//}
 }  
 
-game();
+
